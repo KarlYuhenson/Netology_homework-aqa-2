2dfe8948c0eccdc1cf92db34bfb5e3c538b13b01
Kharitonov Konstantin <karlyuhenson@gmail.com>
2020-02-25 22:41:41 +0300

    version #1

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        System.out.println(actual);
+        System.out.println(actual == expected);
+    }
+}
